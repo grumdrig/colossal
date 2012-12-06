d4d6d706822ee857c5dbb8a6ae9389e21c60c086
Eric Fredricksen <efredricksen@gmail.com>
2012-12-05 19:44:42 -0800

    Turn all furniture and NPCs into objects. Change describe() semantics.

--- adventure.py
+++ adventure.py
@@ -6,31 +6,29 @@ import sys, getopt, textwrap
 
 
 ROOMS = {}
-FURNITURE = {}
+
 
 class Room:
   def __init__(self, name, description, exits,
-               furniture=None, resources=None, inhabitants=None, items=None):
+               resources=None, items=None):
     self.name = name
     self.description = description
     self.exits = exits
-    self.furniture = furniture or {}
+    self.furniture = {}
     self.resources = resources or []
-    self.inhabitants = inhabitants or []
+    self.inhabitants = {}
     self.items = items or []
     self.visited = False
     self.capacity = float('inf')
     ROOMS[name] = self
 
   def describe(self, brief=False):
-    say(self.name)
+    result = self.name
     if not brief:
-      say()
-      say(textwrap.fill(self.description))
-      if self.items:
-        say()
-        for item in self.location.items:
-          say('There is', an(item), 'here.')
+      result += '\n' + textwrap.fill(self.description)
+      for item in self.items:
+        result += '\nThere is ' + an(item) + ' here.'
+    return result
           
 
 Room('Outside of a small house',
@@ -43,11 +41,7 @@ Room('Outside of a small house',
 
 Room('Inside the small house',
      'The house is decorated in an oppressively cozy country style. There are needlepoints on every wall and pillow, and the furniture is overstuffed and outdated. Against one wall there is a trophy case.',
-     { 'out': 'Outside of a small house' },
-     furniture = {
-       'trophy case': {'open': False, 'locked': True, 'contains': []},
-       }
-     )
+     { 'out': 'Outside of a small house' })
 
 Room('Dirt road',
      'You stand on a dirt road running east-west. It\'s just a dirt road. Not much more to say about it than that. Should I mention the bees and butterflies again?',
@@ -59,9 +53,7 @@ Room('Dirt road',
 
 Room('Tar pit',
      'The road leads to a noxious pit of tar. Amidst the tar, out of reach, a tar-encrusted T-rex bobs, half-submerged.',
-     { 'east': 'Outside of a small house.' },
-     furniture = { 'tar pit': {'contains': []} },
-    )
+     { 'east': 'Outside of a small house.' });
 
 Room('Fork in the road',
      'The road leading in from the west forks here. The northeast fork seems to head towards a rocky, hilly area. The road to the southeast is narrower and lined with tall grass.',
@@ -72,46 +64,69 @@ Room('Fork in the road',
 
 Room('Grassy knoll',
      'A path leading from the northwest gives onto a grassy knoll. Upon the knoll is a greasy gnoll. A gnoll is a cross between a gnome and a troll. This particular gnoll is named Bograt, and Bograt, I am sorry to tell you, is a jerk.',
-     { 'northwest': 'Fork in the road' },
-     inhabitants = ['Bograt']
-     )
+     { 'northwest': 'Fork in the road' })
 
 
 class Furniture:
   def __init__(self, name, description, location,
-               capacity=0, contents=None):
+               capacity=0, contents=None, closed=None, locked=None):
     self.name = name
     self.description = description
     self.capacity = capacity
     self.items = contents or []
+    self.closed = closed
+    self.locked = locked
     ROOMS[location].furniture[name] = self
 
   def describe(self, brief=False):
-    if brief:
-      say(an(self.name))
-    else:
-      say(self.description)
+    return 'the ' + self.name if brief else self.description
+
 
 Furniture('mailbox',
-          'A fairly ordinary mailbox, used mostly to mail receive mail. The kind with a flag on the side and so forth. The number "428" is proudly emblazoned with vinyl stickers on one side.',
+          'A fairly ordinary mailbox, used mostly to receive mail. The kind with a flag on the side and so forth. The number "428" is proudly emblazoned with vinyl stickers on one side.',
           'Outside of a small house',
           capacity = 2,
+          closed = True,
           contents = ['letter'])
 
-class Item:
-  def __init__(self, name, mass=False):
-    self.name = name
-    self.mass = mass
+Furniture('trophy case',
+          'This handsome trophy case features space to display up to three treasured items.',
+          'Inside the small house',
+          capacity = 3,
+          closed = True,
+          locked = True);
+
+Furniture('tar pit',
+          'The tar pit emits noxious fumes and bubbles langoriously from time to time.',
+          'Tar pit',
+          capacity = float('inf'));
+
 
 MASS_NOUNS = ['dirt'];
 
-NPCs = {
-  'Bograt': {
-    'description': 'Bograt is a greasy gnoll who lives on a grassy knoll. No two ways about it: he is a jerk.'
-    },
-  }
 
+class Npc:
+  def __init__(self, name, description, location):
+    self.name = name
+    self.description = description
+    ROOMS[location].inhabitants[name.lower()] = self
 
+  def describe(self):
+    return self.description
+    
+
+Npc('Bograt',
+    'Bograt is a greasy gnoll who lives on a grassy knoll. No two ways about it: he is a jerk.',
+    'Grassy knoll');
+
+ITEMS = {}
+
+class Item:
+  def __init__(self, name, description, mass=False):
+    self.name = name
+    self.description = description
+    self.mass = mass
+    
 
 ALIASES = {
   'walk': 'go',
@@ -138,14 +153,11 @@ ALIASES = {
 
 class Player:
   def __init__(self):
-    self.location = ROOMS['Dirt road']
+    self.location = ROOMS['Outside of a small house']
     self.items = []
     self.capacity = 9
     self.dead = False
-
-    self.go('west')  # kinda weasly way to get to starting spot
-    say()
-    self.inventory()
+    say(self.location.describe())
 
   def describe(self):
     return 'You are you. That\'s just who you are.'
@@ -157,19 +169,10 @@ class Player:
       return self.location
     elif item in self.location.furniture:
       return self.location.furniture[item]
+    elif item in self.location.inhabitants:
+      return self.location.inhabitants[item]
     else:
       say('What ' + item + '?')
-
-  def look(self, item, brief=False):
-    item.describe()
-    say(self.location.name)
-    if not brief:
-      say()
-      say(textwrap.fill(self.location.description))
-      if self.location.items:
-        say()
-        for item in self.location.items:
-          say('There is', an(item), 'here.')
 
   def inventory(self):
     if self.items:
@@ -181,7 +184,7 @@ class Player:
 
   def go(self, direction):
     self.location = ROOMS[self.location.exits[direction]]
-    self.location.describe(self.location.visited)
+    say(self.location.describe(self.location.visited))
     self.location.visited = True
 
   def drop(self, item):
@@ -214,6 +217,7 @@ def move(item, source, dest):
   say('What', item, 'now?')
   return False
 
+
 def an(item):
   if item in MASS_NOUNS:
     return 'some ' + item
@@ -232,23 +236,27 @@ def say(*args):
   if VERBOSE:
     print Cap(' '.join(args))
 
+
 def parse(player, line):
   ALIASES['this'] = (player.items[-1:]+['this'])[0]
   ALIASES['that'] = (player.location.items[-1:]+['that'])[0]
-  words = [ALIASES.get(word,word) for word in line.lower().split()]
+  words = [ALIASES.get(word,word) for word in line.strip().lower().split()]
   if not words:
-    say('Huh?')
-    return
+    return True
   command = words.pop(0)
 
-  if command == 'inventory':
+  if command == 'quit':
+    say('Goodbye!')
+    return False
+
+  elif command == 'inventory':
     player.inventory()
 
   elif command == 'look':
-    player.location.describe()
+    say(player.location.describe())
 
   elif command == 'examine':
-    player.resolve(words[0]).describe()
+    say(player.resolve(words[0]).describe())
 
   elif command == 'go':
     player.go(words.pop(0))
@@ -279,7 +287,7 @@ def parse(player, line):
   elif command == 'put' and len(words) >= 3 and words[1] == 'in':
     dest = player.location.furniture[words[2]]
     if move(words[0], player, dest):
-      say('Now', words[0], 'is in', words[2] + '.')
+      say('You put the', words[0], 'in the', words[2] + '.')
     
   elif command in player.location.exits.keys():
     # just a direction. "go" is implied
@@ -287,16 +295,20 @@ def parse(player, line):
 
   else:
     say('I did not understand that command.')
+
+  return True
     
 
 def execute(player, file=None):
   if file:
     for line in file.readlines():
-      say('\n>', line)
-      parse(player, line)
+      say('\n>', line.strip())
+      if not parse(player, line):
+        break
   else:
     while not player.dead:
-      parse(player, raw_input('\n> '))
+      if not parse(player, raw_input('\n> ')):
+        break
 
 
 def main():
